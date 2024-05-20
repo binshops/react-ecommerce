76330667e220b0c1f94536ccf3aced01de75f17d
shaghayegh <shaghayegh.ahmadi1223@gmail.com>
2024-05-20 15:08:59 -0700

    add footer

---
+++ src/const/FooterLink.ts
@@ -0,0 +1,20 @@
+export const footerLink = [
+    {
+      id: 1,
+      footerTitle: "titlw",
+      link: [
+        { title: "title1", link: "link1" },
+        { title: "title2", link: "link1" },
+        { title: "title3", link: "link1" },
+      ],
+    },
+    {
+      id: 2,
+      footerTitle: "title2",
+      link: [
+        { title: "title4", link: "link1" },
+        { title: "title2", link: "link1" },
+        { title: "title3", link: "link1" },
+      ],
+    },
+  ];
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/component/footer";
 import Header from "@/component/header";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
@@ -7,5 +8,6 @@ export default function App({ Component, pageProps }: AppProps) {
   return <>
   <Header />
    <Component {...pageProps} />
+   <Footer />
   </>
 }
