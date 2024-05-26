705467d53cfe11b26aad6c718166972f00c88e43
shaghayegh <shaghayegh.ahmadi1223@gmail.com>
2024-05-26 13:41:21 -0700

    improve home transformer

---
+++ src/utils/type/home.ts
@@ -0,0 +1,13 @@
+export interface HomeAPI {
+  psdata: {
+    featuredProductsList: {
+      id_product: string;
+      name: string;
+      price: string;
+      cover: { url: string };
+      discount_amount: string;
+      quantity: string;
+      rate: number;
+    }[];
+  };
+}
