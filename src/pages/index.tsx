1b1fc8ebe825026e6d2c60b1f33b9c740a382148
shaghayegh <shaghayegh.ahmadi1223@gmail.com>
2024-05-26 13:36:02 -0700

    improve home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import Link from "next/link";
 import MainSlider from "@/component/mainSlider";
 import HomeCategory from "@/component/homeCategory";
@@ -11,16 +7,21 @@ import { HomePageAPI } from "@/const/endPoint";
 import { HomeTransformer } from "@/utils/transformer/home";
 import Subscribe from "@/component/subscribe";
 import InstagramPost from "@/component/instagramPost";
+import { Product } from "@/utils/type";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home({data , homeProductCarousel}) {
+export default function Home({
+  data,
+  homeProductCarousel,
+}: {
+  data: any;
+  homeProductCarousel: Product[];
+}) {
   return (
     <>
-    {console.log(data , homeProductCarousel)}
+      {console.log(data, homeProductCarousel)}
       <MainSlider />
       <HomeCategory />
-      <ProductCarousel product={homeProductCarousel}/>
+      <ProductCarousel product={homeProductCarousel} />
       <Subscribe />
       <InstagramPost />
       <Link href={"/product"}> product </Link>
@@ -29,9 +30,7 @@ export default function Home({data , homeProductCarousel}) {
 }
 
 export async function getServerSideProps() {
-
   const data = await getData(HomePageAPI);
-  const {homeProductCarousel}=HomeTransformer(data);
-  return { props: {data, homeProductCarousel } };
-
+  const { homeProductCarousel } = HomeTransformer(data);
+  return { props: { data, homeProductCarousel } };
 }
