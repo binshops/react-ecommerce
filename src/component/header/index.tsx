7362674a5c89ca3502273cefc698f4059a017902
shaghayegh <shaghayegh.ahmadi1223@gmail.com>
2024-05-26 15:58:21 -0700

    fixed header

--- src/component/header/index.tsx
+++ src/component/header/index.tsx
@@ -1,14 +1,16 @@
-import React, { FC } from 'react';
-import logo from './../../../public/images/logo.png';
-import styles from './header.module.scss';
-import Image from 'next/image';
+import React, { FC } from "react";
+import logo from "./../../../public/images/logo.png";
+import styles from "./header.module.scss";
+import Image from "next/image";
+import Search from "../search";
 
 const Header: FC = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logoWrapper}> 
-        <Image src={logo} alt="" />                     
+      <div className={styles.logoWrapper}>
+        <Image src={logo} alt="logo" />
       </div>
+      <Search />
     </header>
   );
 };
