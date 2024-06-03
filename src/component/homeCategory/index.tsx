94c33ddb2a8ce578a9e37234007c8313255f9b54
shaghayegh <shaghayegh.ahmadi1223@gmail.com>
2024-06-03 05:16:59 -0700

    responsive category

--- src/component/homeCategory/index.tsx
+++ src/component/homeCategory/index.tsx
@@ -2,18 +2,53 @@ import React, { FC } from "react";
 import styles from "./homeCategory.module.scss";
 import Image from "next/image";
 import Link from "next/link";
-import banner1 from "../../../public/images/home/category/banner1.jpg";
-import banner2 from "../../../public/images/home/category/banner2.jpg";
+import { mobileBanners } from "@/const/categoryImage";
+import { desktopBanners } from "@/const/categoryImage";
 
 const HomeCategory: FC = () => {
   return (
     <div className={styles.categoryContainer}>
-      <Link href={"/"} className={styles.categoryItem}>
-        <Image src={banner1} alt="" className={styles.image} />
-      </Link>
-      <Link href={"/"} className={styles.categoryItem}>
-        <Image src={banner2} alt="" className={styles.image} />
-      </Link>
+      <div className={styles.mobileCategory}>
+        {mobileBanners.map((mobileBanner, idx) => {
+          return (
+            <Link
+              href={mobileBanner.link}
+              className={styles.categoryItem}
+              key={idx}
+            >
+              <Image
+                src={mobileBanner.image}
+                alt=""
+                className={styles.image}
+                width={100}
+                height={100}
+              />
+            </Link>
+          );
+        })}
+      </div>
+
+      <div className={styles.desktopCategory}>
+        {desktopBanners.map((desktopBanner, idx) => {
+          return (
+            <Link
+              href={desktopBanner.link}
+              className={`${styles.categoryItem} ${
+                styles[desktopBanner.class]
+              }`}
+              key={idx}
+            >
+              <Image
+                src={desktopBanner.image}
+                alt=""
+                className={styles.image}
+                width={100}
+                height={100}
+              />
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 };
