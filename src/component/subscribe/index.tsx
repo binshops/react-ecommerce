3b67c2b98050b8e48a9008350ce59496a31df857
shaghayegh <shaghayegh.ahmadi1223@gmail.com>
2024-06-03 05:36:25 -0700

    responsive subscription

--- src/component/subscribe/index.tsx
+++ src/component/subscribe/index.tsx
@@ -4,10 +4,13 @@ import styles from "./subscribe.module.scss";
 const Subscribe: FC = () => {
   return (
     <div className={styles.subscribe}>
-      <p className={styles.title}>Subscribe to Newsletters</p>
-      <p className={styles.description}>
-        Be aware of upcoming sales and events. Receive gifts and special offers!
-      </p>
+      <div>
+        <p className={styles.title}>Subscribe to Newsletters</p>
+        <p className={styles.description}>
+          Be aware of upcoming sales and events. Receive gifts and special
+          offers!
+        </p>
+      </div>
       <button className={styles.submit}>Subscribe</button>
     </div>
   );
