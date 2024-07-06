b3f5bdd5bccded77d625b48717dd02bed99ca27e
shaghayegh <shaghayegh.ahmadi1223@gmail.com>
2024-07-06 12:19:38 -0700

    fix request

--- src/pages/product/[slug].tsx
+++ src/pages/product/[slug].tsx
@@ -1,24 +1,34 @@
-import { ProductDetailAPI } from '@/const/endPoint';
-import { getData } from '@/utils/fetchData';
-import { GetServerSidePropsContext } from 'next';
-import React from 'react';
+import ProductGallery from "@/component/productGallery";
+import { ProductDetailAPI } from "@/const/endPoint";
+import { getData } from "@/utils/fetchData";
+import { GetServerSidePropsContext } from "next";
+import React, { FC } from "react";
+import ProductDetails from "@/component/productDetails";
+import { ProductTransformer } from "@/utils/transformer/product";
+import { ProductPageProps } from "@/utils/type";
 
-const ProductDetail = ({ product }) => {
-
+const ProductPage: FC<ProductPageProps> = ({ product }) => {
+  console.log("product", product);
   return (
     <div>
-      <h1>{product.psdata?.name}</h1>
+      <ProductDetails
+        title={product.title}
+        images={product.images}
+        options={product.options}
+        price={product.price}
+        description={product.description}
+      />
     </div>
   );
 };
 
-export async function getServerSideProps(context:GetServerSidePropsContext) {
-  const productId = context.query.slug; 
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const productId = context.query.slug;
 
-  const product =productId&& await getData(ProductDetailAPI , {'product_id':productId});
-
+  const productData =
+    productId && (await getData(ProductDetailAPI, { product_id: productId }));
+  const product = ProductTransformer(productData);
   return { props: { product } };
 }
 
-
-export default ProductDetail;
+export default ProductPage;
--- src/utils/type/product.ts
+++ src/utils/type/product.ts
@@ -9,9 +9,63 @@ export interface Product {
 }
 
 export interface ProductCarouselProps {
-    product: Product[];
-  }
+  product: Product[];
+}
 
-  export interface ProductCardProps {
-    product: Product;
-  }
+export interface ProductCardProps {
+  product: Product;
+}
+
+export interface productInfo {
+  title: string;
+  price: string;
+  options:ProductOptions[]
+  description:string
+}
+export interface ProductOptions {
+  id: number;
+  title: string;
+  items: {
+    id: number;
+    value: string;
+    hex_value?:string
+  }[];
+}
+
+export interface productGalleryProps {
+  images: { src: string }[];
+}
+
+export interface productDetailsProps {
+  title: string;
+  images: { src: string }[];
+  price:string
+  options:ProductOptions[]
+  description:string
+}
+export interface ProductAPI {
+  psdata: {
+    name: string;
+    images: { src: string }[];
+    options: {
+      id: number;
+      title: string;
+      items: {
+        id: number;
+        value: string;
+      }[];
+    }[];
+    description:string,
+    price:string
+  };
+}
+
+export interface ProductPageProps {
+  product: {
+    title: string;
+    images: { src: string }[];
+    price: string;
+    options: ProductOptions[];
+    description:string;
+  };
+}
