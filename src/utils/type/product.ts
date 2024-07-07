f41a450291bf1bfe288aa2e2fffa9c5377c2e2dc
shaghayegh <shaghayegh.ahmadi1223@gmail.com>
2024-07-06 19:49:15 -0700

    fix type of product

--- src/utils/type/product.ts
+++ src/utils/type/product.ts
@@ -17,10 +17,12 @@ export interface ProductCardProps {
 }
 
 export interface productInfo {
+  id: string;
   title: string;
   price: string;
-  options:ProductOptions[]
-  description:string
+  options: ProductOptions[];
+  description: string;
+  setProduct: Function;
 }
 export interface ProductOptions {
   id: number;
@@ -28,7 +30,7 @@ export interface ProductOptions {
   items: {
     id: number;
     value: string;
-    hex_value?:string
+    hex_value?: string;
   }[];
 }
 
@@ -37,35 +39,45 @@ export interface productGalleryProps {
 }
 
 export interface productDetailsProps {
-  title: string;
-  images: { src: string }[];
-  price:string
-  options:ProductOptions[]
-  description:string
-}
-export interface ProductAPI {
-  psdata: {
-    name: string;
-    images: { src: string }[];
-    options: {
-      id: number;
-      title: string;
-      items: {
-        id: number;
-        value: string;
-      }[];
-    }[];
-    description:string,
-    price:string
-  };
+  product: ProductType;
+  setProduct: Function;
 }
 
 export interface ProductPageProps {
-  product: {
-    title: string;
-    images: { src: string }[];
-    price: string;
-    options: ProductOptions[];
-    description:string;
-  };
+  data: ProductType;
+}
+
+export interface GroupAttributes {
+  attributes: Record<string, { name: string; html_color_code?: string }>;
+}
+export interface Group {
+  attributes: GroupAttributes["attributes"];
+  group_name: string;
+}
+export interface Groups {
+  [key: string]: Group;
+}
+
+export interface ProductData {
+  name: string;
+  images: { src: string }[];
+  default_image: { url: string };
+  price: string;
+  groups: Groups;
+  options?: ProductOptions[]; // This should ideally be replaced with a more specific type
+  description: string;
+  id_product: string;
+}
+
+export interface ProductAPI {
+  psdata: ProductData;
+}
+
+export interface ProductType {
+  title: string;
+  images: { src: string }[];
+  price: string;
+  options: ProductOptions[];
+  description: string;
+  id: string;
 }
