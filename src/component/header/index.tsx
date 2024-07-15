d8391e439e815bfaab1be8dd4f86e3ba437c0a6b
shaghayegh <shaghayegh.ahmadi1223@gmail.com>
2024-07-14 18:41:12 -0700

    add home link

--- src/component/header/index.tsx
+++ src/component/header/index.tsx
@@ -4,13 +4,16 @@ import styles from "./header.module.scss";
 import Image from "next/image";
 import Search from "../search";
 import MegaMenu from "../megaMenu";
+import Link from "next/link";
 
 const Header: FC = () => {
   return (
     <header className={styles.header}>
       <div className={`${styles.headerContent} container`}>
         <div className={styles.logoWrapper}>
+          <Link href={'/'}>
           <Image src={logo} alt="logo" />
+          </Link>
         </div>
         <MegaMenu />
         <Search />
