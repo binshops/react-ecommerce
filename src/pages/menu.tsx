cc62cc8c02e0d0492c57461ad89155673e2fa8db
shaghayegh <shaghayegh.ahmadi1223@gmail.com>
2024-07-18 15:07:23 -0700

    create menu page

---
+++ src/pages/menu.tsx
@@ -0,0 +1,37 @@
+// pages/basket.tsx
+import React, { useEffect, useState } from "react";
+import { getData } from "@/utils/fetchData";
+import { MegaMenuTransformer } from "@/utils/transformer/megaMenu";
+import { MegaMenuAPI } from "@/const/endPoint";
+import { menuItems } from "@/utils/type";
+import AccordionItem from "@/component/accordionItem";
+
+const MenuPage = () => {
+  const [menuItems, setMenuItems] = useState<menuItems[]>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getData(MegaMenuAPI);
+      const transformedData = MegaMenuTransformer(data);
+      setMenuItems(transformedData.menuItems);
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <div>
+      {menuItems?.map((item) => {
+        return (
+          <AccordionItem
+            title={item.label}
+            links={item.children}
+            mode="dark"
+            key={item.id}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default MenuPage;
