8755ccafaa6626789a231c6243e4f2849cf0f063
shaghayegh <shaghayegh.ahmadi1223@gmail.com>
2024-07-23 12:25:45 -0700

    add sort component

---
+++ src/component/category/sort/index.tsx
@@ -0,0 +1,54 @@
+import React, { FC, useEffect, useState } from "react";
+import styles from "./sort.module.scss";
+import { SortProps } from "../category.types";
+import arrow from "./../../../../public/images/icon/Chevron.png";
+import Image from "next/image";
+import { getData } from "@/utils/fetchData";
+import { CategoryAPI } from "@/const/endPoint";
+import { CategoryTransformer } from "@/utils/transformer/category";
+
+const Sort: FC<SortProps> = ({ sortOptions, setCategory, categoryId }) => {
+  const activeSort = sortOptions?.find((item) => item.isActive === true);
+  const [orderQuery, setOrderQuery] = useState<string>();
+  const [showOption, setOption] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const productData = await getData(CategoryAPI, {
+          id_category: categoryId,
+          page: 1,
+          order: orderQuery,
+        });
+        const transformedData = CategoryTransformer(productData);
+        setCategory(transformedData);
+      } catch (error) {
+        console.error("Failed to fetch product data:", error);
+      }
+    };
+    fetchData();
+    setOption(false);
+  }, [orderQuery]);
+  return (
+    <div className={styles.sortWrapper}>
+      <div className={styles.activeItem} onClick={() => setOption(!showOption)}>
+        <p className={styles.title}>{activeSort?.label}</p>
+        <Image src={arrow} alt="arrow" />
+      </div>
+      <div className={`${styles.sortOptions} ${showOption ? styles.show : ""}`}>
+        {sortOptions.map((item, index) => {
+          return (
+            <div
+              onClick={() => setOrderQuery(item.querySort)}
+              key={index}
+              className={styles.item}
+            >
+              <p>{item.label}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Sort;
