1b5f9713ffc98ff5822f3255fba0fa2ce31bccb6
shaghayegh <shaghayegh.ahmadi1223@gmail.com>
2024-08-13 12:01:13 -0700

    responsive footer

--- src/component/footer/index.tsx
+++ src/component/footer/index.tsx
@@ -2,20 +2,79 @@ import React, { FC } from "react";
 import styles from "./footer.module.scss";
 import AccordionItem from "../accordionItem";
 import { footerLink } from "@/const/FooterLink";
+import useWindowSize from "@/utils/hooks/useWindowSize";
+import { useRouter } from "next/router";
+import faceBook from "./../../../public/images/icon/faceBook.png";
+import twitter from "./../../../public/images/icon/twitter.png";
+import pinterest from "./../../../public/images/icon/pinterest.png";
+import youtube from "./../../../public/images/icon/youtube.png";
+import Image from "next/image";
 
 const Footer: FC = () => {
+  const { width } = useWindowSize();
+  const router = useRouter();
+
   return (
     <div className={styles.footer}>
-      {footerLink.map((item) => {
-        return (
-          <AccordionItem
-            titleLink={item.footerLink}
-            title={item.footerTitle}
-            links={item.link}
-            key={item.id}
-          />
-        );
-      })}
+      {width < 768 ? (
+        footerLink.map((item) => {
+          return (
+            <AccordionItem
+              titleLink={item.footerLink}
+              title={item.footerTitle}
+              links={item.link}
+              key={item.id}
+            />
+          );
+        })
+      ) : (
+        <>
+          <div className={styles.columnWrapper}>
+            {footerLink.map((item) => {
+              return (
+                <div
+                  className={styles.column}
+                  onClick={() => router.push(item.footerLink)}
+                >
+                  <div className={styles.titleRow}>
+                    <p className={styles.title}>{item.footerTitle}</p>
+                  </div>
+                  <div className={styles.linkBox}>
+                    {item.link?.map((link) => {
+                      return (
+                        <a
+                          href={link.link}
+                          key={link.title}
+                          className={styles.subLink}
+                        >
+                          {link.title}
+                        </a>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className={styles.socialBox}>
+            <p className={styles.title}>SOCIAL</p>
+            <div className={styles.items}>
+              <a href="/" className={styles.link}>
+                <Image src={faceBook} alt="faceBook" />
+              </a>
+              <a href="/" className={styles.link}>
+                <Image src={pinterest} alt="pinterest" />
+              </a>
+              <a href="/" className={styles.link}>
+                <Image src={twitter} alt="twitter" />
+              </a>
+              <a href="/" className={styles.link}>
+                <Image src={youtube} alt="youtube" />
+              </a>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
