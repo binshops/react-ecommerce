0a369dc34b993031854a9b0ab84e39cf55bcd13d
shaghayegh <shaghayegh.ahmadi1223@gmail.com>
2024-08-20 12:59:13 -0700

    fix mobile menu

--- src/component/navigationBar/index.tsx
+++ src/component/navigationBar/index.tsx
@@ -1,11 +1,20 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import styles from "./navigationBar.module.scss";
 import Image from "next/image";
 import Link from "next/link";
 import { navigationItems } from "@/const/navigationBar";
+import { useMegaMenu } from "@/context/menuContext";
+import { useScrollLock } from "@/utils/hooks/useScrollLock";
+import Modal from "../modal";
+import AccordionItem from "../accordionItem";
 
 const NavigationBar: FC = () => {
+  const { lockScroll, unlockScroll } = useScrollLock();
+  const [openMenu, setOpenMenu] = useState(false);
+  const megaMenuContext = useMegaMenu();
+  const menu = megaMenuContext?.menu;
   return (
+    <>
     <div className={styles.navigationBar}>
       {navigationItems.map((item, idx) => {
         return (
@@ -13,6 +22,11 @@ const NavigationBar: FC = () => {
             className={`${styles.navigationItem}`}
             href={item.link}
             key={idx}
+            onClick={(e) => {
+              e.preventDefault();
+              setOpenMenu(true);
+              lockScroll();
+            }}
           >
             <div className={`${item.text === "Cart" ? styles.cartImage : ""}`}>
               <Image src={item.icon} alt={item.text} className={styles.image} />
@@ -23,7 +37,31 @@ const NavigationBar: FC = () => {
           </Link>
         );
       })}
+    
     </div>
+    <Modal
+        isOpen={openMenu}
+        onClose={() => {
+          setOpenMenu(false);
+          unlockScroll();
+        }}
+        isFullScreen
+      >
+        <div>
+          {menu?.map((item) => {
+            return (
+              <AccordionItem
+                title={item.label}
+                links={item.children}
+                titleLink={item.link}
+                mode="dark"
+                key={item.id}
+              />
+            );
+          })}
+        </div>
+      </Modal>
+    </>
   );
 };
 
