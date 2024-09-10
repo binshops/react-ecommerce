fdf546f03c3f7f522c8e8625d04ff32a1f5b86cc
shaghayegh <shaghayegh.ahmadi1223@gmail.com>
2024-09-09 22:44:06 -0700

    add cart item component

---
+++ src/component/cartContent/cardItem/index.tsx
@@ -0,0 +1,49 @@
+import React, { FC } from "react";
+import styles from "./cardItem.module.scss";
+import { useCart } from "@/context/cartContext";
+import toast from "react-hot-toast";
+import UpdateQuantity from "./updateQuantity";
+import { CartItemProps } from "../cartContent.types";
+
+const CartItem: FC<CartItemProps> = ({ product }) => {
+  const { id, productAttributeId, quantity, name, image, price, attributes } =
+    product;
+  const { removeFromCart } = useCart();
+  const handleRemove = async () => {
+    const item = {
+      id: id,
+      productAttributeId: productAttributeId,
+    };
+    removeFromCart(item);
+    toast.success("Successfully remove from cart");
+  };
+  return (
+    <div className={styles.cardItem}>
+      <div className={styles.image}>
+        <img src={image} alt={name} />
+      </div>
+      <div className={styles.content}>
+        <p className={styles.title}>{name}</p>
+        <div className={styles.priceBox}>
+          <p className={styles.price}>{price}</p>
+        </div>
+        {Object.entries(attributes)?.map(([key, value], index) => (
+          <div className={styles.attribute} key={index}>
+            <span className={styles.attributeName}>{key}: </span>
+            <span className={styles.attributeValue}>{value}</span>
+          </div>
+        ))}
+        <UpdateQuantity
+          id={id}
+          productAttributeId={productAttributeId}
+          quantity={quantity}
+        />
+        <button className={styles.removeItem} onClick={() => handleRemove()}>
+          remove from cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
