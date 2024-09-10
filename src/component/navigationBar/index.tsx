84ca4357b0d5494cffb8a3a9163b8cbda208edde
shaghayegh <shaghayegh.ahmadi1223@gmail.com>
2024-09-09 22:45:28 -0700

    add cart to navigation bar

--- src/component/navigationBar/index.tsx
+++ src/component/navigationBar/index.tsx
@@ -9,16 +9,21 @@ import AccordionItem from "../accordionItem";
 import icon1 from "./../../../public/images/icon/home.png";
 import icon2 from "./../../../public/images/icon/Menu.png";
 import icon4 from "./../../../public/images/icon/cart.png";
+import { useCart } from "@/context/cartContext";
+import CartContent from "../cartContent";
+
 const NavigationBar: FC = () => {
   const { lockScroll, unlockScroll } = useScrollLock();
   const [openMenu, setOpenMenu] = useState(false);
+  const [openCart, setOpenCart] = useState(false);
   const megaMenuContext = useMegaMenu();
   const menu = megaMenuContext?.menu;
+  const { cart } = useCart();
+
   return (
     <>
       <div className={styles.navigationBar}>
-
-      <div
+        <div
           className={`${styles.navigationItem}`}
           onClick={(e) => {
             e.preventDefault();
@@ -37,13 +42,20 @@ const NavigationBar: FC = () => {
           </div>
           <p>Home</p>
         </Link>
-        <Link className={`${styles.navigationItem}`} href={"/card"}>
-
+        <div
+          className={`${styles.navigationItem}`}
+          onClick={(e) => {
+            e.preventDefault();
+            setOpenCart(true);
+            lockScroll();
+          }}
+        >
           <div className={styles.cartImage}>
+            {cart?.products.length > 0 && <div className={styles.badge}></div>}
             <Image src={icon4} alt="Card" className={styles.image} />
           </div>
           <p className={styles.cartItem}>Card</p>
-        </Link>
+        </div>
       </div>
       <Modal
         isOpen={openMenu}
@@ -67,6 +79,7 @@ const NavigationBar: FC = () => {
           })}
         </div>
       </Modal>
+      <CartContent isOpen={openCart} setIsOpen={setOpenCart} />
     </>
   );
 };
