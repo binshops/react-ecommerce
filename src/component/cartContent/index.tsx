79385f649d4ee0fa8625180bb1217ef7fe107b6e
shaghayegh <shaghayegh.ahmadi1223@gmail.com>
2024-09-09 22:45:08 -0700

    add cart content component

---
+++ src/component/cartContent/index.tsx
@@ -0,0 +1,35 @@
+import React, { FC } from "react";
+import styles from "./cartContent.module.scss";
+import { useCart } from "@/context/cartContext";
+import CartItem from "./cardItem";
+import Modal from "../modal";
+import { useScrollLock } from "@/utils/hooks/useScrollLock";
+import { CartContentProps } from "./cartContent.types";
+
+const CartContent: FC<CartContentProps> = ({ isOpen, setIsOpen }) => {
+  const { cart } = useCart();
+  const { unlockScroll } = useScrollLock();
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={() => {
+        setIsOpen(false);
+        unlockScroll();
+      }}
+      isFullScreen
+    >
+      <div className={styles.cart}>
+        <p className={styles.title}>
+          Total items:
+          <span>{cart?.totalProduct}</span>
+        </p>
+        {cart?.products?.map((product, idx) => {
+          return <CartItem product={product} key={idx} />;
+        })}
+      </div>
+    </Modal>
+  );
+};
+
+export default CartContent;
