b66f2eed0befe99967cd4a80a3d53039481b3d60
shaghayegh <shaghayegh.ahmadi1223@gmail.com>
2024-09-29 02:17:39 -0700

    fix search bug

--- src/component/search/index.tsx
+++ src/component/search/index.tsx
@@ -65,11 +65,20 @@ const Search: FC = () => {
             handleInputChange(e.target.value);
           }}
         />
-        <img
-          src="/images/icon/Search.png"
-          alt="searchIcon"
-          className={styles.icon}
-        />
+        {value.length === 0 ? (
+          <img
+            src="/images/icon/Search.png"
+            alt="searchIcon"
+            className={styles.icon}
+          />
+        ) : (
+          <img
+            src="/images/icon/darkClose.png"
+            alt="searchIcon"
+            className={styles.icon}
+            onClick={() => setValue("")}
+          />
+        )}
       </div>
       <div
         className={`${styles.searchResult} ${
