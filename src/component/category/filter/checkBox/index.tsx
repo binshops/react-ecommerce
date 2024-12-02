c9f296452579ece76a9ea01ac23f1e34c9819810
shaghayegh <shaghayegh.ahmadi1223@gmail.com>
2024-12-02 20:14:32 +0330

    review checkbox component

--- src/component/category/filter/checkBox/index.tsx
+++ src/component/category/filter/checkBox/index.tsx
@@ -1,36 +1,37 @@
 import React, { FC } from "react";
-import styles from "./checkBox.module.scss";
-import { CheckBoxProps } from "./checkBox.types";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
+import { CheckBoxProps } from "./checkBox.types";
+
+import styles from "./checkBox.module.scss";
+
 const CheckBox: FC<CheckBoxProps> = ({ filter, setFilterQuery }) => {
   return (
-    <>
-      <div className={styles.content}>
+    <div className={styles.content}>
+      <div
+        className={styles.check}
+        onClick={() => {
+          setFilterQuery(
+            filter.filterQuery !== "" ? filter.filterQuery : "undefined"
+          );
+        }}
+      >
         <div
-          className={styles.check}
-          onClick={() => {
-            setFilterQuery(
-              filter.filterQuery !== "" ? filter.filterQuery : "undefined"
-            );
+          className={`${styles.box} ${filter.active ? styles.active : ""}`}
+          style={{
+            backgroundColor: filter.properties.color
+              ? filter.properties.color
+              : undefined,
           }}
         >
-          <div
-            className={`${styles.box} ${filter.active ? styles.active : ""}`}
-            style={{
-              backgroundColor: filter.properties.color
-                ? filter.properties.color
-                : undefined,
-            }}
-          >
-            {filter.active && <FontAwesomeIcon icon={faCheck} color="#fff"/>}
-          </div>
-          <p className={styles.label}>{filter.label}</p>
-          <p className={styles.count}>( {filter.productCount} )</p>
+          {filter.active && <FontAwesomeIcon icon={faCheck} color="#fff" />}
         </div>
+        <p className={styles.label}>{filter.label}</p>
+        <p className={styles.count}>( {filter.productCount} )</p>
       </div>
-    </>
+    </div>
   );
 };
 
