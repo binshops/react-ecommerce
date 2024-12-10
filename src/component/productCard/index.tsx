a1d65a2398cf658ae68ef07cf91fa4be356e6d0f
shaghayegh <shaghayegh.ahmadi1223@gmail.com>
2024-12-10 17:15:55 +0330

    review product card component

--- src/component/productCard/index.tsx
+++ src/component/productCard/index.tsx
@@ -1,23 +1,27 @@
 import React, { FC } from "react";
-import styles from "./productCard.module.scss";
-import "swiper/css";
-import "swiper/css/pagination";
-import { ProductCardProps } from "@/utils/type";
-import Price from "../product/price";
 import Link from "next/link";
+
+import { ProductCardProps } from "@/utils/type";
+
+import Price from "../product/price";
+
+import styles from "./productCard.module.scss";
 
 const ProductCard: FC<ProductCardProps> = ({ product }) => {
   return (
-    <Link href={`/product/${product.id}`} >
-
-    <div className={styles.productCard}>
-      {product.image && (
-        <img className={styles.image} src={product.image} alt={product.name} />
-      )}
-      <h3 className={styles.name}>{product.name}</h3>
-      <Price price={product.price} />
-    </div>
-      </Link>
+    <Link href={`/product/${product.id}`}>
+      <div className={styles.productCard}>
+        {product.image && (
+          <img
+            className={styles.image}
+            src={product.image}
+            alt={product.name}
+          />
+        )}
+        <h3 className={styles.name}>{product.name}</h3>
+        <Price price={product.price} />
+      </div>
+    </Link>
   );
 };
 
