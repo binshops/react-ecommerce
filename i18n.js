0a904324a3838777029f232aa7561c31ab33c861
shaghayegh <shaghayegh.ahmadi1223@gmail.com>
2024-12-17 12:28:27 +0330

    fix config

--- i18n.js
+++ i18n.js
@@ -1,12 +1,12 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "./locales/en/common.json";
-import fa from "./locales/fa/common.json";
+import fr from "./locales/fr/common.json";
 
 i18n.use(initReactI18next).init({
   resources: {
     en: { translation: en },
-    fa: { translation: fa },
+    fa: { translation: fr },
   },
   lng: "en",
   fallbackLng: "en",
